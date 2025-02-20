9754ad4f4d9c4006b1985a58f113e4c3b7a13eba
Jack G King <jackking@evnode.ib.cluster>
2025-02-20 13:09:38 -0500

    models folder

---
+++ scripts/launch_jupyter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -J jupyter
+#SBATCH --time=0-12:00
+#SBATCH -n 1
+#SBATCH --gres=gpu:a100:1
+###SBATCH --gres=gpu:1
+#SBATCH --mem 50G
+###SBATCH --partition=evlab
+#SBATCH -o jupyter.out
+#SBATCH --exclude=node111
+###SBATCH --exclude=node105
+
+source ~/.bashrc
+
+conda activate superurop
+
+unset XDG_RUNTIME_DIR
+
+PORT=8081
+
+
+jupyter lab --ip=0.0.0.0 --port=${PORT} --no-browser --NotebookApp.allow_origin='*' --NotebookApp.port_retries=0
